b2369583ebe08022b435ede82dde98f3c7b0aad9
Fariha Mehzabin <henlothere45@gmail.com>
2022-11-22 20:48:32 +0600

    Add review Scroll option

--- index.js
+++ index.js
@@ -1,27 +1,98 @@
 import { data } from "/data.js";
 
 let reviewDiv = document.getElementById("reviews");
+let nextPic = 1;
 
-function getData() {
-  let html = " ";
+// function getData() {
+//   let html = " ";
 
-  data.forEach((el) => {
-    html += `<div class="review-container hidden" id="review-${el.id}">
-        <div class="review-img">
-        <img src="/images/image-${el.id}.jpg" alt="">
-        </div>
-    <h4>${el.review}</h4>
-    <div class="reviewer-info">
-    <h5>${el.name}</h5>
-    <h6>${el.position}</h6>
-    </div>
-  </div>`;
-  });
+//   data.forEach((el) => {
+//     html += `<div class="review-container hidden" id="review-${el.id}">
+//         <div class="review-img">
+//         <img src="/images/image-${el.id}.jpg" alt="">
+//         <div class="slide">
+//         <svg class="prev" id='prev' data-rev='${el.id}' xmlns="http://www.w3.org/2000/svg" width="13" height="18"><path fill="none" stroke="#8585AC" stroke-width="3" d="M11 1L3 9l8 8"/></svg>
+//         <svg class="next" id="next" data-rev='${el.id}' xmlns="http://www.w3.org/2000/svg" width="13" height="18"><path fill="none" stroke="#8585AC" stroke-width="3" d="M2 1l8 8-8 8"/></svg>
+//         </div>
+//         </div>
+//     <h4>${el.review}</h4>
+//     <div class="reviewer-info">
+//     <h5>${el.name}</h5>
+//     <h6>${el.position}</h6>
+//     </div>
+//   </div>`;
+//   });
 
-  reviewDiv.innerHTML = html;
+//   reviewDiv.innerHTML = html;
 
-  let start = document.getElementById("review-1").classList.remove("hidden");
-  console.log(start);
-}
+//   let start = document.getElementById("review-1").classList.remove("hidden");
+// //   console.log(start);
+// }
 
-getData();
+// getData();
+
+let nextBtn = document.getElementsByClassName("next");
+
+nextBtn[0].addEventListener("click", function (e) {
+//   let x = e.target.dataset.rev;
+
+  let idk = document.getElementById("review-1");
+  let idk1 = document.getElementById("review-2");
+
+  idk.classList.toggle("hidden");
+  idk1.classList.toggle("hidden");
+
+
+  console.log(idk);
+  console.log(idk1);
+
+
+//   if(nextPic === data.length){
+//     nextPic = 1;
+//     document.getElementById(`review-${nextPic}`).classList.toggle("hidden");
+//     document.getElementById(`review-${nextPic+1}`).classList.toggle("hidden");
+//   }
+//   else {
+//     nextPic++;
+//     document.getElementById(`review-${nextPic}`).classList.toggle("hidden");
+//     document.getElementById(`review-${x}`).classList.toggle("hidden");
+
+//   }
+  
+  console.log("ugh")
+
+//   console.log(x,data.length,nextPic);
+  
+});
+
+
+nextBtn[1].addEventListener("click", function (e) {
+  //   let x = e.target.dataset.rev;
+
+  let idk = document.getElementById("review-1");
+  let idk1 = document.getElementById("review-2");
+
+    idk.classList.toggle("hidden");
+  idk1.classList.toggle("hidden");
+
+  console.log(idk);
+  console.log(idk1);
+
+  //   if(nextPic === data.length){
+  //     nextPic = 1;
+  //     document.getElementById(`review-${nextPic}`).classList.toggle("hidden");
+  //     document.getElementById(`review-${nextPic+1}`).classList.toggle("hidden");
+  //   }
+  //   else {
+  //     nextPic++;
+  //     document.getElementById(`review-${nextPic}`).classList.toggle("hidden");
+  //     document.getElementById(`review-${x}`).classList.toggle("hidden");
+
+  //   }
+
+  console.log("ugh");
+
+  //   console.log(x,data.length,nextPic);
+});
+
+
